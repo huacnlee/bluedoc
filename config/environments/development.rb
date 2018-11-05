20226165e2415dc9178e87748e6a67840141f22b
Jason Lee <huacnlee@gmail.com>
2018-11-05 15:04:28 +0800

    Add Aliyun Storage support

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :amazon
+  config.active_storage.service = :aliyun
   # config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
