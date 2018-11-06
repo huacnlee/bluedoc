af43936d36e3f5d5d3766a8cf99c8482714df321
Jason Lee <huacnlee@gmail.com>
2018-11-06 11:47:17 +0800

    avatar url use /uploads for avoid difference private url flash bug

--- app/models/user/avatar_dependency.rb
+++ app/models/user/avatar_dependency.rb
@@ -24,7 +24,7 @@ class User
 
     style = :small unless AVATAR_STYLES.include?(style)
 
-    self.avatar.service_url(expires_in: 1.day)
+    "/uploads/#{self.avatar.blob.key}?s=#{style}"
   rescue
     self.default_avatar
   end
