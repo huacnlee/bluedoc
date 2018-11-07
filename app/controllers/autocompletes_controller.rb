e2c7b4dbc69c339152a36174ed05367861ad9035
Jason Lee <huacnlee@gmail.com>
2018-11-07 17:11:27 +0800

    Member autocomplete fix popup list style

--- app/controllers/autocompletes_controller.rb
+++ app/controllers/autocompletes_controller.rb
@@ -3,7 +3,7 @@ class AutocompletesController < ApplicationController
 
   # GET /autocomplete/users
   def users
-    @users = User.prefix_search(params[:q], user: current_user)
+    @users = User.prefix_search(params[:q], user: current_user, limit: 10)
     render "users", layout: false
   end
 end
