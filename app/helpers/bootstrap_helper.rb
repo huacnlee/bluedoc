a654c37ea5480d5bbefa0dcff57b77f000bd2c5f
Jason Lee <huacnlee@gmail.com>
2018-11-09 14:24:54 +0800

    Fix Repository settings advanced page tab active

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -15,21 +15,29 @@ module BootstrapHelper
     opts[:check_parameters] = false if opts[:check_parameters].nil?
 
     yield(list) if block_given?
+
     items = []
     list.each do |link|
       urls = link.match(/href=(["'])(.*?)(\1)/) || []
-      url = urls.length > 2 ? urls[2] : nil
+      url  = urls.length > 2 ? urls[2] : nil
+
+      controller_names = link.match(/data-controller=(["'])(.*?)(\1)/) || []
+      c_name = controller_names.length > 2 ? controller_names[2] : nil
+
       if url && current_page?(url, check_parameters: opts[:check_parameters])
         link = link.gsub(opts[:class], "#{opts[:class]} #{opts[:active_class]}")
       end
 
-      item = link
-      if opts[:type] == :li
-        item = content_tag("li", raw(item), class: "nav-item")
+      if c_name && controller_name.to_s == c_name
+        link = link.gsub(opts[:class], "#{opts[:class]} #{opts[:active_class]}")
       end
+
+      item = link
+      item = content_tag("li", raw(item), class: "nav-item") if opts[:type] == :li
 
       items << item
     end
+
     raw items.join("")
   end
 end
