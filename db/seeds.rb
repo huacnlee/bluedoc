6c4dcb1e59586b9e26dbcabb24cfc1ebc6c0c867
Jason Lee <huacnlee@gmail.com>
2018-11-13 16:22:32 +0800

    Create default user, group in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if User.count == 0
+  admin = User.create!(slug: "admin", email: "admin@booklab.io", password: "123456", password_confirmation: "123456")
+end
+
+if Group.count == 0
+  Group.create!(slug: "sample", name: "Sample Group", creator_id: User.first.id)
+end
