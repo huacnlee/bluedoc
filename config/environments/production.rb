92e8ddfc3a36a0834ce61969290a20e9acbf2147
Jason Lee <huacnlee@gmail.com>
2018-11-15 17:37:48 +0800

    Setup smtp mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,7 @@ Rails.application.configure do
   config.active_job.queue_name_prefix = "booklab"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.smtp_settings = Setting.mailer_options.deep_symbolize_keys
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
