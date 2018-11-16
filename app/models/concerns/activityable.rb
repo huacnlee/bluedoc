81fc49dcace439cedaae6dbb98c175a928e2a69e
Jason Lee <huacnlee@gmail.com>
2018-11-16 15:06:04 +0800

    Depends destroy notifications

--- app/models/concerns/activityable.rb
+++ app/models/concerns/activityable.rb
@@ -11,17 +11,23 @@ module Activityable
     case self.class.name
     when "Group"
       Activity.where(group_id: self.id).delete_all
+      Notification.where(group_id: self.id).delete_all
     when "User"
       if self.type == "Group"
         Activity.where(group_id: self.id).delete_all
+        Notification.where(group_id: self.id).delete_all
       else
         Activity.where(user_id: self.id).delete_all
+        Notification.where(user_id: self.id).delete_all
         Activity.where(actor_id: self.id).delete_all
+        Notification.where(actor_id: self.id).delete_all
       end
     when "Repository"
       Activity.where(repository_id: self.id).delete_all
+      Notification.where(repository_id: self.id).delete_all
     end
 
     Activity.where(target: self).delete_all
+    Notification.where(target: self).delete_all
   end
 end
--- test/models/activityable_test.rb
+++ test/models/activityable_test.rb
@@ -12,13 +12,20 @@ class ActivityableTest < ActiveSupport::TestCase
     create_list(:activity, 2, group_id: group.id)
     create(:activity, target: group)
 
+    create_list(:notification, 2, group_id: group.id)
+    create(:notification, target: group)
+
     assert_equal 2, Activity.where(group_id: group.id).count
     assert_equal 1, Activity.where(target: group).count
+    assert_equal 2, Notification.where(group_id: group.id).count
+    assert_equal 1, Notification.where(target: group).count
 
     group.destroy
     assert_equal 0, Activity.where(group_id: group.id).count
     assert_equal 0, Activity.where(target: group).count
     assert_not_equal 0, Activity.count
+    assert_equal 0, Notification.where(group_id: group.id).count
+    assert_equal 0, Notification.where(target: group).count
   end
 
   test "depend destroy User" do
@@ -26,29 +33,44 @@ class ActivityableTest < ActiveSupport::TestCase
     create_list(:activity, 2, user_id: user.id)
     create_list(:activity, 2, actor_id: user.id)
     create(:activity, target: user)
+    create_list(:notification, 2, user_id: user.id)
+    create_list(:notification, 2, actor_id: user.id)
+    create(:notification, target: user)
 
     assert_equal 2, Activity.where(user_id: user.id).count
     assert_equal 2, Activity.where(actor_id: user.id).count
     assert_equal 1, Activity.where(target: user).count
+    assert_equal 2, Notification.where(user_id: user.id).count
+    assert_equal 2, Notification.where(actor_id: user.id).count
+    assert_equal 1, Notification.where(target: user).count
 
     user.destroy
     assert_equal 0, Activity.where(user_id: user.id).count
     assert_equal 0, Activity.where(actor_id: user.id).count
     assert_equal 0, Activity.where(target: user).count
     assert_not_equal 0, Activity.count
+    assert_equal 0, Notification.where(user_id: user.id).count
+    assert_equal 0, Notification.where(actor_id: user.id).count
+    assert_equal 0, Notification.where(target: user).count
   end
 
   test "depend destroy Repository" do
     repo = create(:repository)
     create_list(:activity, 2, repository_id: repo.id)
     create(:activity, target: repo)
+    create_list(:notification, 2, repository_id: repo.id)
+    create(:notification, target: repo)
 
     assert_equal 2, Activity.where(repository_id: repo.id).count
     assert_equal 1, Activity.where(target: repo).count
+    assert_equal 2, Notification.where(repository_id: repo.id).count
+    assert_equal 1, Notification.where(target: repo).count
 
     repo.destroy
     assert_equal 0, Activity.where(repository_id: repo.id).count
     assert_equal 0, Activity.where(target: repo).count
     assert_not_equal 0, Activity.count
+    assert_equal 0, Notification.where(repository_id: repo.id).count
+    assert_equal 0, Notification.where(target: repo).count
   end
 end
