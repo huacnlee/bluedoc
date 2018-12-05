807064d95eb7c3924fd00e93b97e41675b96fab4
Jason Lee <huacnlee@gmail.com>
2018-12-05 16:03:33 +0800

    Add wide mode

--- app/javascript/booklab/reader/index.js
+++ app/javascript/booklab/reader/index.js
@@ -1,5 +1,52 @@
 import BodyToc from "./body_toc";
 
 document.addEventListener("turbolinks:load", () => {
+  if ($(".doc-page").length == 0) {
+    return;
+  }
+
+  // print
+  $(".reader-body").on("click", ".btn-print-doc", (e) => {
+    e.preventDefault();
+    window.print();
+  });
+  // wide mode
+  $(".reader-body").on("click", ".btn-wide-mode", (e) => {
+    e.preventDefault();
+    // GoInFullscreen($(".reader-body")[0]);
+    $(".reader-body").addClass("reader-wide-mode");
+  });
+
+
+  // wide mode
+  $(".reader-body").on("click", ".btn-wide-mode-exit", (e) => {
+    e.preventDefault();
+    $(".reader-body").removeClass("reader-wide-mode");
+    // GoOutFullscreen();
+  });
+
   BodyToc.init();
 });
+
+
+function GoInFullscreen(element) {
+	if(element.requestFullscreen)
+		element.requestFullscreen();
+	else if(element.mozRequestFullScreen)
+		element.mozRequestFullScreen();
+	else if(element.webkitRequestFullscreen)
+		element.webkitRequestFullscreen();
+	else if(element.msRequestFullscreen)
+		element.msRequestFullscreen();
+}
+
+function GoOutFullscreen() {
+	if(document.exitFullscreen)
+		document.exitFullscreen();
+	else if(document.mozCancelFullScreen)
+		document.mozCancelFullScreen();
+	else if(document.webkitExitFullscreen)
+		document.webkitExitFullscreen();
+	else if(document.msExitFullscreen)
+		document.msExitFullscreen();
+}
