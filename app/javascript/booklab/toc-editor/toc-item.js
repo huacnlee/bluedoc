651504a21681ef93cd591dd397c9bc05e43f44ca
yangguoquan <guoquan.yangvip@gmail.com>
2018-12-06 19:29:26 +0800

    Fix TocEdit input  can't focus (#19)
    * Fix some spell
    * Add some devDependencies
    * Fix TocEdit input  can't focus
    * Fix input value cache

--- app/javascript/booklab/toc-editor/toc-item.js
+++ app/javascript/booklab/toc-editor/toc-item.js
@@ -4,196 +4,112 @@ import { SortableContainer, SortableHandle, SortableElement, arrayMove } from 'r
 const DragHandle = SortableHandle(() => <span className="draghandle"><i className="fas fa-news-feed"></i></span>); // This can be any component you want
 
 const TocItemElement = SortableElement(({
-  item,
-  editing,
+  item: {url = '', title = '', depth },
   onIndent,
   onUnindent,
-  onShowEditing,
   onDeleteItem,
   onKeyPressEdit,
-  onChangeField,
   onChange,
-  onBlurEdit
 }) => {
-  let url = item.url || "";
-  let title = item.title || "";
-  let urlClass = "";
-  let titleClass = "";
-
-  if (url.trim().length === 0) {
-    url = "no-link";
-    urlClass = "placeholder";
-  }
-  if (title.trim().length === 0) {
-    title = "New item";
-    titleClass = "placeholder";
-  }
-
   return (
-    <div className={`toc-item-drageable toc-item toc-item-d${item.depth}`}>
-    <DragHandle />
-    <div className="indent-buttons">
-      <a href="#" onClick={onUnindent} className="indent-left mr-2"><i class="fas fa-left"></i></a>
-      <a href="#" onClick={onIndent} className="indent-right"><i class="fas fa-right"></i></a>
-    </div>
-    <a href="#" onClick={onDeleteItem} className="btn-delete"><i class="fas fa-minus"></i></a>
-
-    {editing === "" && (
-      <div>
-        <div className={`title item-editable ${titleClass}`} data-field="title" onClick={onShowEditing}>{title}</div>
-        <div className={`slug item-editable ${urlClass}`} data-field="url" onClick={onShowEditing}>{url}</div>
-      </div>
-    )}
-
-    {editing !== "" && (
-      <div>
+    <div className={`toc-item-drageable toc-item toc-item-d${depth}`}>
+      {/* drag */}
+      <DragHandle />
+      {/* indentation */}
+      <div onClick={onUnindent} className="indent-left mr-2"><i class="fas fa-left"></i></div>
+      <div onClick={onIndent} className="indent-right"><i class="fas fa-right"></i></div>
+      {/* delete */}
+      <div onClick={onDeleteItem} className="btn-delete"><i class="fas fa-minus"></i></div>
+      {/* show && edit */}
+      <form className={'cell-wrap'}>
         <input type="text"
-            onBlur={onBlurEdit}
-            onChange={onChange}
-            onKeyUp={onKeyPressEdit}
-            data-field="title"
-            placeholder="title"
-            ref={(input) => { input && editing === "title" && input.focus() }}
-            className="form-inplace-edit title"
-            value={item.title} />
+          onChange={onChange}
+          onKeyUp={onKeyPressEdit}
+          data-field="title"
+          placeholder="title"
+          className="form-edit title"
+          value={title} />
         <input type="text"
-            onBlur={onBlurEdit}
-            onChange={onChange}
-            onKeyUp={onKeyPressEdit}
-            data-field="url"
-            placeholder="url"
-            ref={(input) => { input && editing === "url" && input.focus() }}
-            className="form-inplace-edit slug"
-            value={item.url} />
-      </div>
-    )}
+          onChange={onChange}
+          onKeyUp={onKeyPressEdit}
+          data-field="url"
+          placeholder="url"
+          className="form-edit slug"
+          value={url} />
+      </form>
     </div>
   );
 });
 
 
-export default class TocItem extends React.Component {
+export default class TocItem extends React.PureComponent {
   constructor(props) {
     super(props);
-
-    this.state = {
-      item: props.item,
-      editing: (props.item.isNew == true ? "title" : ""),
-    };
   }
 
   // indent
-  indentItem = (depth) => {
-    const item = this.state.item;
-    if (!item.depth) {
-      item.depth = 0;
+  indentItem = (step) => {
+    const { item } = this.props;
+    let depth = item.depth;
+    if (!depth) {
+      depth = 0;
     }
-    if (depth == 1 && item.depth >= 5) {
+    if (depth == 1 && depth >= 5) {
       return false;
     }
-    if (depth == -1 && item.depth == 0) {
+    if (depth == -1 && depth == 0) {
       return false;
     }
-
-    item.depth += depth;
-
-    this.setState({ item: item });
-    this.saveChange(item);
+    depth += step;
+    this.saveChange({...item, depth});
   }
 
-  saveChange = (item) => {
+  saveChange = (item = this.props.item) => {
     this.props.onChangeItem(this.props.index, item);
   }
 
   // indent
-  onIndent = (e) => {
-    e.preventDefault();
-    this.indentItem(1);
-  }
+  onIndent = (e) => this.indentItem(1)
 
   // unindent
-  onUnindent = (e) => {
-    e.preventDefault();
-    this.indentItem(-1);
-  }
+  onUnindent = (e) => this.indentItem(-1)
 
-  // switch to editing mode
-  onShowEditing = (e) => {
-    e.preventDefault();
-
-    const input = e.currentTarget;
-    const field = input.getAttribute("data-field");
-
-    this.setState({ editing: field });
-  }
-
+  // sync
   onChangeField = (e) => {
     e.preventDefault();
     const input = e.currentTarget;
     const field = input.getAttribute("data-field");
-
-    const item = this.state.item;
-    if (field === "title") {
-      item.title = input.value;
-    } else {
-      item.url = input.value;
-    }
-
-    this.setState({ item: item });
+    this.saveChange({
+      ...this.props.item,
+      [field]: input.value
+    })
   }
 
   // remove item
-  onDeleteItem = (e) => {
-    e.preventDefault();
-    this.props.onDelete(this.props.index);
-  }
-
-  onBlurEdit = (e) => {
-    e.preventDefault();
-    this.onChangeField(e);
-    this.saveChange(this.state.item);
-    this.switchReadonly(e);
-  }
-
-  switchReadonly = (e) => {
-    e.preventDefault();
-    this.setState({ editing: "" });
-  }
+  onDeleteItem = () => this.props.onDelete(this.props.index)
 
   onKeyPress = (e) => {
     const input = e.currentTarget;
-    const field = input.getAttribute("data-field");
-
     if (e.keyCode === 13) {
       // enter
-      this.onChangeField(e);
-      this.saveChange(this.state.item);
-      this.switchReadonly(e);
-      return;
+      input.blur();
     } else if (e.keyCode === 27) {
       // esc
-      this.switchReadonly(e);
       // FIXME: to cancel edit
     }
   }
 
   render() {
-    this.state.item = this.props.item;
-
+    const {index, item} = this.props;
     return (
       <TocItemElement
-        {...this.props}
-        item={this.state.item}
-        editing={this.state.editing}
+        index={index}
+        item={item}
         onIndent={this.onIndent}
         onChange={this.onChangeField}
         onUnindent={this.onUnindent}
-        onShowEditing={this.onShowEditing}
         onKeyPressEdit={this.onKeyPress}
-        onBlurEdit={this.onBlurEdit}
         onDeleteItem={this.onDeleteItem} />
     )
   }
 }
-
-
