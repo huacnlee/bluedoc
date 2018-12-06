56eeada4d8167f3cca9f49f3f773e0d811130556
Jason Lee <huacnlee@gmail.com>
2018-12-06 11:39:53 +0800

    Add fork code link to BookLab::RichText::Attribute

--- lib/booklab/rich_text/attribute.rb
+++ lib/booklab/rich_text/attribute.rb
@@ -1,5 +1,7 @@
 module BookLab
   module RichText
+    # Fork ActionText has_rich_text method from:
+    # https://github.com/rails/actiontext/blob/18974137be2c4027dfcf430668edd712e7ec011b/lib/action_text/attribute.rb
     module Attribute
       extend ActiveSupport::Concern
 
