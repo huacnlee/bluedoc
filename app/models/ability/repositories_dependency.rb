728440625878d81ad1978150ec5c88da3a17cf87
Jason Lee <huacnlee@gmail.com>
2018-12-07 12:37:40 +0800

    Add ability for Repository collaborators

--- app/models/ability/repositories_dependency.rb
+++ app/models/ability/repositories_dependency.rb
@@ -4,9 +4,19 @@ class Ability
   def abilities_for_repositories
     can :read, Repository, privacy: :public
 
+    # :admin
+    can :manage, Repository do |repo|
+      repo.user_role(user) == :admin
+    end
     can :manage, Repository do |repo|
       can? :manage, repo.user
     end
+
+    # :editor
+    can %i[read create_doc], Repository do |repo|
+      repo.user_role(user) == :editor
+    end
+
     can %i[update], Repository do |repo|
       can? :update, repo.user
     end
@@ -18,6 +28,16 @@ class Ability
         role.in? %i[editor admin]
       end
     end
+
+    # :reader
+    can %i[read], Repository do |repo|
+      if repo.public?
+        return true
+      else
+        repo.user_role(user) == :reader
+      end
+    end
+
     can %i[read], Repository do |repo|
       if repo.public?
         true
--- test/models/ability/repositories_test.rb
+++ test/models/ability/repositories_test.rb
@@ -88,4 +88,46 @@ class Ability::RepositoriesTest < ActiveSupport::TestCase
     assert @anonymous_ability.can? :read, repo
     assert @anonymous_ability.cannot? :read, private_repo
   end
+
+  test "Membered Repository" do
+    repo = create(:repository, privacy: :public)
+    private_repo = create(:repository, privacy: :private)
+
+    # :reader
+    private_repo.add_member(@user, :reader)
+    assert @ability.cannot? :manage, repo
+    assert @ability.cannot? :update, repo
+    assert @ability.cannot? :create_doc, repo
+    assert @ability.can? :read, repo
+
+    assert @ability.cannot? :manage, private_repo
+    assert @ability.cannot? :update, private_repo
+    assert @ability.cannot? :create_doc, private_repo
+    assert @ability.can? :read, private_repo
+
+    # :editor
+    private_repo.add_member(@user, :editor)
+    assert @ability.cannot? :manage, private_repo
+    assert @ability.cannot? :update, private_repo
+    assert @ability.can? :create_doc, private_repo
+    assert @ability.can? :read, private_repo
+
+    # :admin
+    private_repo.add_member(@user, :admin)
+    assert @ability.can? :manage, private_repo
+
+    # other user
+    assert @other_ability.cannot? :create, private_repo
+    assert @other_ability.cannot? :update, private_repo
+    assert @other_ability.cannot? :destroy, private_repo
+    assert @other_ability.cannot? :create_doc, private_repo
+    assert @other_ability.cannot? :read, private_repo
+
+    # anonymous
+    assert @anonymous_ability.cannot? :create, private_repo
+    assert @anonymous_ability.cannot? :update, private_repo
+    assert @anonymous_ability.cannot? :destroy, private_repo
+    assert @anonymous_ability.cannot? :create_doc, private_repo
+    assert @anonymous_ability.cannot? :read, private_repo
+  end
 end
