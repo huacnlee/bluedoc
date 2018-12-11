89300d2ad394e1e026c9de57389ddc477ebe7738
Jason Lee <huacnlee@gmail.com>
2018-12-11 17:22:40 +0800

    Resize image to 50% width for Retina Display

--- app/javascript/primer-booklab/index.js
+++ app/javascript/primer-booklab/index.js
@@ -4,6 +4,7 @@ import "./dialog/index";
 import "./flash/index";
 import "./confirm/index";
 import "./clipboard/index";
+import "./retina-image/index";
 
 import '@github/markdown-toolbar-element';
 
---
+++ app/javascript/primer-booklab/retina-image/index.js
@@ -0,0 +1,18 @@
+// Resize image to 50% size for Retina Display
+document.addEventListener("turbolinks:load", () => {
+  const bindMarkdownBodyImageToRetina = () => {
+    $(".markdown-body img").on("load", (e) => {
+      const img = e.delegateTarget;
+      if (!img.getAttribute("width") && img.naturalWidth) {
+        const width = img.naturalWidth;
+        img.setAttribute("width", width / 2);
+      }
+    })
+  }
+
+  bindMarkdownBodyImageToRetina();
+  // after xhr: true success, bind event again.
+  document.addEventListener("ajax:success", () => {
+    bindMarkdownBodyImageToRetina();
+  });
+})
