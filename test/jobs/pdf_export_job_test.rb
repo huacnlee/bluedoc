1b8268fe9a095b08965b58aa77738e5e0b579a94
Jason Lee <huacnlee@gmail.com>
2018-12-14 11:36:05 +0800

    Render Repository PDF with Toc style, and add page break after each doc section

--- test/jobs/pdf_export_job_test.rb
+++ test/jobs/pdf_export_job_test.rb
@@ -46,6 +46,9 @@ class PDFExportJobTest < ActiveSupport::TestCase
 
   test "perform with Repository without TOC" do
     repo = create(:repository)
+    repo.preferences[:has_toc] = false
+    repo.save
+
     doc0 = create(:doc, repository: repo, body: "Doc 0")
     doc1 = create(:doc, repository: repo, body: "Doc 1")
     doc2 = create(:doc, repository: repo, body: "Doc 2")
@@ -81,6 +84,70 @@ class PDFExportJobTest < ActiveSupport::TestCase
 
     assert_html_equal doc0.title, html_node.css(".markdown-body .doc-section:last-child .doc-title").inner_text
     assert_html_equal doc0.body_html, html_node.css(".markdown-body .doc-section:last-child .section-body").inner_html
+
+    assert_equal "done", repo.export_pdf_status.value
+    assert_equal true, repo.pdf.attached?
+    assert_equal repo.pdf_filename, repo.pdf.blob.filename.to_s
+
+    wicked.expect(:pdf_from_string, f) do |args|
+      raise "Error"
+    end
+    repo.export_pdf_status = "running"
+    assert_changes -> { ExceptionTrack::Log.count }, 1 do
+      WickedPdf.stub(:new, wicked) do
+        PDFExportJob.perform_now(repo)
+      end
+    end
+    assert_equal "done", repo.export_pdf_status.value
+  end
+
+  test "perform with Repository with TOC" do
+    repo = create(:repository)
+    doc0 = create(:doc, repository: repo, body: "Doc 0")
+    doc1 = create(:doc, repository: repo, body: "Doc 1")
+    doc2 = create(:doc, repository: repo, body: "Doc 2")
+    doc3 = create(:doc, repository: repo, body: "Doc 3")
+    doc4 = create(:doc, repository: repo, body: "Doc 4")
+
+    toc = <<~TOC
+    - url: #{doc4.slug}
+    - url: #{doc3.slug}
+    - url: #{doc0.slug}
+    - url: #{doc2.slug}
+    - url: https://google.com
+    TOC
+    repo.update!(toc: toc)
+
+    f = WickedPdf::WickedPdfTempfile.new("blank.png")
+
+    rendered_html = ""
+    wicked = MiniTest::Mock.new
+    wicked.expect(:pdf_from_string, f) do |html, opts|
+      rendered_html = html
+    end
+
+    WickedPdf.stub(:new, wicked) do
+      PDFExportJob.perform_now(repo)
+    end
+    wicked.verify
+
+    assert_not_equal "", rendered_html
+
+    html_node = Nokogiri::HTML(rendered_html)
+    assert_equal repo.name, html_node.css("title")[0].inner_text.strip
+    assert_equal repo.name, html_node.css(".pdf-title")[0].inner_text.strip
+
+    assert_equal 4, html_node.css(".markdown-body .doc-section").size
+    assert_equal 4, html_node.css(".markdown-body .doc-title").size
+    assert_equal 4, html_node.css(".markdown-body .section-body").size
+    assert_html_equal doc4.title, html_node.css(".markdown-body .doc-section .doc-title")[0].inner_text
+    assert_html_equal doc4.body_html, html_node.css(".markdown-body .doc-section .section-body")[0].inner_html
+    assert_html_equal doc3.title, html_node.css(".markdown-body .doc-section .doc-title")[1].inner_text
+    assert_html_equal doc3.body_html, html_node.css(".markdown-body .doc-section .section-body")[1].inner_html
+    assert_html_equal doc0.title, html_node.css(".markdown-body .doc-section .doc-title")[2].inner_text
+    assert_html_equal doc0.body_html, html_node.css(".markdown-body .doc-section .section-body")[2].inner_html
+    assert_html_equal doc2.title, html_node.css(".markdown-body .doc-section:last-child .doc-title").inner_text
+    assert_html_equal doc2.body_html, html_node.css(".markdown-body .doc-section:last-child .section-body").inner_html
 
     assert_equal "done", repo.export_pdf_status.value
     assert_equal true, repo.pdf.attached?
