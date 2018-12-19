7f72aa44c8b8b71354319c293cb1cfa33654d64b
Jason Lee <huacnlee@gmail.com>
2018-12-19 11:06:03 +0800

    Filter password and body in parameters

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -3,4 +3,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [:password, :toc, :draft_body, :body, :body_sml, :draft_body_sml, :password_confirmation]
