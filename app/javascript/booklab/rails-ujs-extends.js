3ac5fbd240fb73346c251a7d436e69e0b220e12e
Jason Lee <huacnlee@gmail.com>
2018-12-19 19:57:09 +0800

    Add handler for Rails UJS ajax:error to give a right message.

--- app/javascript/booklab/rails-ujs-extends.js
+++ app/javascript/booklab/rails-ujs-extends.js
@@ -6,5 +6,24 @@ document.addEventListener("turbolinks:load", () => {
   }).on("ajax:complete", (event) => {
     $el = $(event.currentTarget);
     $el.removeAttr("disabled");
-  });
+  }).on("ajax:error", (event) => {
+    const status = event.detail[1];
+    const xhr = event.detail[2];
+
+    switch (xhr.status) {
+    case 401:
+      alert("You need to sign in or sign up before continuing.");
+      location.href = "/account/sign_in";
+      break;
+    case 403:
+      alert("Access denined, status: 403");
+      break;
+    case 422:
+      alert("Submit failed, please refresh browser and retry it.");
+      break;
+    default:
+      alert("Unknow error with remote submit, status: " + status);
+      break;
+    }
+  })
 })
