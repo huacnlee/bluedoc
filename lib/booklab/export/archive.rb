c721d66aa0bd945a484d479dcff5125806608f3c
Jason Lee <huacnlee@gmail.com>
2019-01-03 12:56:34 +0800

    Download image attachments into Archive zip on export

--- lib/booklab/export/archive.rb
+++ lib/booklab/export/archive.rb
@@ -12,7 +12,9 @@ module BookLab
 
         repository.docs.each do |doc|
           body = "# #{doc.title}\n\n#{doc.body_plain}"
-          body = public_attachment(body)
+          body_html = doc.body_html
+
+          body = downlod_images(body, body_html)
           write_file!("docs/#{doc.slug}.md", body)
         end
 
@@ -24,8 +26,53 @@ module BookLab
         FileUtils.rm_rf(zip_filename) if defined? zip_filename
       end
 
-      def public_attachment(body)
-        body.gsub("](/uploads/", "](#{Setting.host}/uploads/")
+      # Download images in body into ../images/
+      def downlod_images(body, body_html)
+        images_dir = File.join(repo_dir, "images")
+        FileUtils.mkdir_p(images_dir)
+
+        doc = Nokogiri::HTML(body_html)
+
+        doc.css("img").each do |node|
+          src = node.attr("src")
+          next if src.blank?
+
+          # match src is /uploads/{key}
+          match = src.match(/\/uploads\/([\w]+)/i)
+          next if match.nil?
+          file_key = match[1]
+          next if file_key.nil?
+
+          # Find blob record for get old filename
+          blob = ActiveStorage::Blob.find_by(key: file_key)
+          next if blob.blank?
+
+          fname = File.join(images_dir, blob.filename.to_s)
+          if File.exists?(fname)
+            # if same filename exist, rename into {key}.ext
+            ext = File.extname(fname)
+            fname = File.join(images_dir, "#{file_key}#{ext}")
+          end
+
+          # Streaming download blob file into {fname}
+          begin
+            File.open(fname, "w")  do |f|
+              blob.download { |data| f.write(data.force_encoding("UTF-8")) }
+            end
+          rescue => e
+            logger.error "Download blob: #{blob.key} error: #{e.inspect}"
+            next
+          end
+
+          # replace old "/uploads/{key}" into new src "../images/{new_filename}"
+          new_src = "../images/#{File.basename(fname)}"
+          logger.info "------- replace image src ----------"
+          logger.info "- #{src}"
+          logger.info "+ #{new_src}"
+          body = body.gsub(src, new_src)
+        end
+
+        body
       end
     end
   end
--- test/lib/booklab/export/archive_test.rb
+++ test/lib/booklab/export/archive_test.rb
@@ -7,9 +7,47 @@ class BookLab::Export::ArchiveTest < ActiveSupport::TestCase
     @repo = create(:repository)
   end
 
-  test "public_attachment" do
-    body = "/uploads/foo ![](/uploads/foo) ![](/aaa/bbb)"
+  test "downlod_images" do
+    file0 = load_file("blank.png")
+
+    file_url0 = BookLab::Blob.upload(file0.path)
+    file_url1 = BookLab::Blob.upload(file0.path)
+    file_key1 = file_url1.match(/uploads\/([\w]+)/)[1]
+
+    body = <<~MD
+    #{file_url0}
+
+    ![](/uploads/)
+
+    ![](#{file_url0})
+
+    ![](#{file_url1})
+
+    ![](https://www.google.com.hk/test.png)
+    MD
+
+    body_html = BookLab::HTML.render(body, format: :markdown)
+
     exporter = BookLab::Export::Archive.new(repository: @repo)
-    assert_equal "/uploads/foo ![](#{Setting.host}/uploads/foo) ![](/aaa/bbb)", exporter.send(:public_attachment, body)
+
+    expected = <<~MD
+    ../images/blank.png
+
+    ![](/uploads/)
+
+    ![](../images/blank.png)
+
+    ![](../images/#{file_key1}.png)
+
+    ![](https://www.google.com.hk/test.png)
+    MD
+    assert_equal expected, exporter.send(:downlod_images, body, body_html)
+
+    images_dir = File.join(exporter.repo_dir, "images")
+    assert_equal true, File.exists?(File.join(images_dir, "blank.png"))
+    assert_equal file0.size, File.size(File.join(images_dir, "blank.png"))
+
+    assert_equal true, File.exists?(File.join(images_dir, "#{file_key1}.png"))
+    assert_equal file0.size, File.size(File.join(images_dir, "#{file_key1}.png"))
   end
 end
