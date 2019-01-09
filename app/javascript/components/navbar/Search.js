20e261c7b0be0db222a959dac6c9e7cfce7a5972
Guoquan Yang <guoquan.yang.vip@gmail.com>
2019-01-09 19:20:51 +0800

    change dashboard menu icon name

--- app/javascript/components/navbar/Search.js
+++ app/javascript/components/navbar/Search.js
@@ -1,18 +1,26 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
 
-class Search extends React.Component {
+class Search extends React.PureComponent {
   render() {
     const { action = '/search', scope, value } = this.props;
     const placeholder = scope ? `Search in ${scope}` : 'Search BookLab';
-
     return (
-    <form action={action || '/search'} className="subnav-search-context" method="GET">
-      <div className="subnav-search">
-        <input name="q" type="text" placeholder={placeholder} autocomplete="off" className="form-control form-search-control subnav-search-input" defaultValue={value} />
-        <i className="fas fa-search subnav-search-icon"></i>
-      </div>
-    </form>
+      <form action={action || '/search'} className="subnav-search-context" method="GET">
+        <div className="subnav-search">
+          <input
+            name="q"
+            type="text"
+            placeholder={placeholder}
+            autocomplete="off"
+            className="form-control form-search-control subnav-search-input"
+            defaultValue={value}
+          />
+          <i className="fas fa-search subnav-search-icon"></i>
+        </div>
+      </form>
     );
   }
 }
