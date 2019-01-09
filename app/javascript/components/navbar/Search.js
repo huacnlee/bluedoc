9c6de865e2e197ef178957910a68795cf1ef47d6
Guoquan Yang <guoquan.yang.vip@gmail.com>
2019-01-09 19:45:21 +0800

    fix search input

--- app/javascript/components/navbar/Search.js
+++ app/javascript/components/navbar/Search.js
@@ -1,27 +1,31 @@
-import React from "react"
-import PropTypes from "prop-types"
+// eslint-disable-next-line import/no-extraneous-dependencies
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types';
 
-class Search extends React.Component {
-  render () {
-    let { action, scope, value } = this.props;
-
-    const placeholder = scope ? `Search in ${scope}` : "Search BookLab";
-    if (!action) {
-      action = "/search"
-    }
-
+class Search extends React.PureComponent {
+  render() {
+    const { action = '/search', scope, value } = this.props;
+    const placeholder = scope ? `Search in ${scope}` : 'Search BookLab';
     return (
-    <form action={action} className="subnav-search-context" method="GET">
-      <div className="subnav-search">
-        <input name="q" type="text" placeholder={placeholder} autocomplete="off" className="form-control form-search-control subnav-search-input" value={value} />
-        <i className="fas fa-search subnav-search-icon"></i>
-      </div>
-    </form>
+      <form action={action || '/search'} className="subnav-search-context" method="GET">
+        <div className="subnav-search">
+          <input
+            name="q"
+            type="text"
+            placeholder={placeholder}
+            autocomplete="off"
+            className="form-control form-search-control subnav-search-input"
+            defaultValue={value}
+          />
+          <i className="fas fa-search subnav-search-icon"></i>
+        </div>
+      </form>
     );
   }
 }
 
 Search.propTypes = {
-  placeholder: PropTypes.string
+  placeholder: PropTypes.string,
 };
-export default Search
+export default Search;
