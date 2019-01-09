c6565b119ca83720e090bff409b728c8c13bf6a3
Jason Lee <huacnlee@gmail.com>
2019-01-09 19:03:51 +0800

    Use booklab-sml 0.2.0 new parser

--- lib/booklab/pipeline/sml_filter.rb
+++ lib/booklab/pipeline/sml_filter.rb
@@ -4,7 +4,9 @@ module BookLab
   class Pipeline
     class SmlFilter < ::HTML::Pipeline::TextFilter
       def call
-        html = BookLab::SML.parse(@text)
+        html = BookLab::SML.parse(@text,
+          plantuml_service_host: Setting.plantuml_service_host,
+          mathjax_service_host: Setting.mathjax_service_host)
         html.strip!
         html
       end
