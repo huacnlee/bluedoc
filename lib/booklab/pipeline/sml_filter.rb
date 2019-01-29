7a6247ee90df464b6d658fdfd554a4d49fec6948
Jason Lee <huacnlee@gmail.com>
2019-01-29 11:11:59 +0800

    Fix SML test

--- lib/booklab/pipeline/sml_filter.rb
+++ lib/booklab/pipeline/sml_filter.rb
@@ -4,9 +4,10 @@ module BookLab
   class Pipeline
     class SmlFilter < ::HTML::Pipeline::TextFilter
       def call
-        BookLab::SML.parse(@text,
+        renderer = BookLab::SML.parse(@text,
           plantuml_service_host: Setting.plantuml_service_host,
           mathjax_service_host: Setting.mathjax_service_host)
+        renderer.to_html
       end
     end
   end
