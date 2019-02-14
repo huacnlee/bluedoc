e7423ef6e0bd04a7d9cb306a8cc7fd6137a0a7a5
Guoquan Yang <guoquan.yang.vip@gmail.com>
2019-02-14 16:40:57 +0800

    fix rails test

--- test/controllers/repository_settings_controller_test.rb
+++ test/controllers/repository_settings_controller_test.rb
@@ -135,7 +135,7 @@ class RepositorySettingsControllerTest < ActionDispatch::IntegrationTest
     assert_equal 200, response.status
 
     assert_select ".box.transfer-docs" do
-      assert_select ".box-header .title", text: "Transfer docs to other repository"
+      assert_select ".box-header p.content", text: "Transfer docs to other repository"
       assert_select ".box-row input[type=checkbox]", 10
     end
   end
@@ -414,7 +414,7 @@ class RepositorySettingsControllerTest < ActionDispatch::IntegrationTest
     assert_equal 200, response.status
 
     assert_select ".box.export-repository-pdf" do
-      assert_select ".box-header .sub-title", text: "Export as PDF"
+      assert_select ".box-header p.content", text: "Export as PDF"
       assert_select ".pdf-export-generate" do
         assert_select ".btn-generate-pdf", text: "Generate PDF" do
           assert_select "[href=?]", repo.to_path("/settings/export?type=pdf&force=1")
@@ -430,7 +430,7 @@ class RepositorySettingsControllerTest < ActionDispatch::IntegrationTest
     assert_equal 200, response.status
 
     assert_select ".box.export-repository-pdf" do
-      assert_select ".box-header .sub-title", text: "Export as PDF"
+      assert_select ".box-header p.content", text: "Export as PDF"
       assert_select ".pdf-export-exist" do
         assert_select ".btn-download-pdf", text: "Download PDF" do
           assert_select "[href=?]", repo.export_url(:pdf)
@@ -449,7 +449,7 @@ class RepositorySettingsControllerTest < ActionDispatch::IntegrationTest
     assert_equal 200, response.status
 
     assert_select ".box.export-repository-pdf" do
-      assert_select ".box-header .sub-title", text: "Export as PDF"
+      assert_select ".box-header p.content", text: "Export as PDF"
       assert_select ".pdf-export-running" do
         assert_select ".pdf-export-retry-message" do
           assert_select "a", text: "retry" do
