262ded27e2fdc32cd3d55e2a28b3061bcb66d921
Jason Lee <huacnlee@gmail.com>
2019-02-22 17:41:33 +0800

    Add RAILS_ASSET_HOST to config asset_host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,9 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  if ENV["RAILS_ASSET_HOST"].present?
+    config.action_controller.asset_host = ENV["RAILS_ASSET_HOST"]
+  end
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
