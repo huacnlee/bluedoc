5351ce7b20013203fe47fb9a2761fd9e95f77bba
Jason Lee <huacnlee@gmail.com>
2019-02-26 17:17:03 +0800

    Fix direct require /js/*.js to supports asset_host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,6 +54,8 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  config.action_controller.asset_host = "http://asset.bluedoc.dev:3000"
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
