20e14d558db09be7c48d5c48179a750cd3722175
Jason Lee <huacnlee@gmail.com>
2019-02-26 20:26:20 +0800

    Fix S3 storage, use private storage

--- config/initializers/active_storage_patch.rb
+++ config/initializers/active_storage_patch.rb
@@ -9,14 +9,14 @@ end
 
 # Override S3 service for public-read ACL upload and url generate
 module ActiveStorageS3ServiceURL
-  def url(key, expires_in:, filename:, disposition:, content_type:)
-    object_for(key).public_url
-  end
+  # def url(key, expires_in:, filename:, disposition:, content_type:)
+  #   object_for(key).public_url
+  # end
 
   def upload(key, io, checksum: nil, **)
     instrument :upload, key: key, checksum: checksum do
       begin
-        object_for(key).put(upload_options.merge(body: io, content_md5: checksum, acl: "public-read", cache_control: "max-age=#{300.days}"))
+        object_for(key).put(upload_options.merge(body: io, content_md5: checksum, cache_control: "max-age=#{300.days}"))
       rescue Aws::S3::Errors::BadDigest
         raise ActiveStorage::IntegrityError
       end
@@ -26,8 +26,7 @@ module ActiveStorageS3ServiceURL
   def url_for_direct_upload(key, expires_in:, content_type:, content_length:, checksum:)
     instrument :url, key: key do |payload|
       generated_url = object_for(key).presigned_url :put, expires_in: expires_in.to_i,
-        content_type: content_type, content_length: content_length, content_md5: checksum,
-        acl: "public-read", cache_control: "max-age=#{300.days}"
+        content_type: content_type, content_length: content_length, content_md5: checksum, cache_control: "max-age=#{300.days}"
 
       payload[:url] = generated_url
 
