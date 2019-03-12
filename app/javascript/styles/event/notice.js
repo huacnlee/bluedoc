e0085cce9df4f533053f4de8e979a95107ee9ead
Guoquan Yang <guoquan.yang@5th.im>
2019-03-12 17:49:04 +0800

    Add notice auto hide after 3s

--- app/javascript/styles/event/notice.js
+++ app/javascript/styles/event/notice.js
@@ -3,4 +3,10 @@ document.addEventListener('turbolinks:load', () => {
     const $container = $(e.delegateTarget);
     $container.remove();
   });
+  setTimeout(() => {
+    const noticeEle = document.querySelector('.navbar-notice .notice');
+    if (noticeEle) {
+      noticeEle.remove();
+    }
+  }, 3000);
 });
