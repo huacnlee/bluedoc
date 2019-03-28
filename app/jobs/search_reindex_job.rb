670dfef4c4ab4d218c669d9fbc90fc9f2bee1a9e
Jason Lee <huacnlee@gmail.com>
2019-03-28 11:34:51 +0800

    Delete indexes on rebuild search index, use default document_type: _doc

--- app/jobs/search_reindex_job.rb
+++ app/jobs/search_reindex_job.rb
@@ -2,6 +2,13 @@
 
 class SearchReindexJob < ApplicationJob
   def perform
+    # https://github.com/elastic/elasticsearch-rails/blob/00596345ec1c22d177603dcdde7d6422a7cf8de7/elasticsearch-model/lib/elasticsearch/model/indexing.rb#L224
+    # Force to recreate new index (Delete first) to ensure that old invalid data will be cleanup.
+    [User, Group, Repository, Doc, Note].each do |klass|
+      klass.__elasticsearch__.delete_index!(force: true)
+      klass.__elasticsearch__.create_index!
+    end
+
     User.where(type: "User").find_each { |record| record.__elasticsearch__.index_document }
     Group.find_each { |record| record.__elasticsearch__.index_document }
     Repository.find_each { |record| record.__elasticsearch__.index_document }
--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -15,7 +15,6 @@ module Searchable
     index_name do
       "#{Rails.env}-#{name.pluralize}".downcase
     end
-    document_type name.underscore
 
     after_commit on: :create do
       self.reindex
