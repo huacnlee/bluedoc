b366fa2ddc53cfeb260ee6e1456496cd611aa5f7
Jason Lee <huacnlee@gmail.com>
2019-03-28 12:08:07 +0800

    Add test for SearchReindexJob delete/recreate indexes

--- test/jobs/search_reindex_job_test.rb
+++ test/jobs/search_reindex_job_test.rb
@@ -4,6 +4,7 @@ require "test_helper"
 
 class SearchReindexJobTest < ActiveSupport::TestCase
   include ActiveJob::TestHelper
+  include JobsTestHelper
 
   test "perform" do
     create_list(:user, 2)
@@ -12,6 +13,30 @@ class SearchReindexJobTest < ActiveSupport::TestCase
     create_list(:doc, 5)
     create_list(:note, 5)
 
-    SearchReindexJob.perform_now
+    # Should invoked delete index
+    assert_perform_request method: "DELETE", url: "test-notes" do
+      assert_perform_request method: "DELETE", url: "test-groups" do
+        assert_perform_request method: "DELETE", url: "test-users" do
+          assert_perform_request method: "DELETE", url: "test-repositories" do
+            assert_perform_request method: "DELETE", url: "test-docs" do
+              SearchReindexJob.perform_now
+            end
+          end
+        end
+      end
+    end
+
+    # Should invoked create index
+    assert_perform_request method: "HEAD", url: "test-notes" do
+      assert_perform_request method: "HEAD", url: "test-groups" do
+        assert_perform_request method: "HEAD", url: "test-users" do
+          assert_perform_request method: "HEAD", url: "test-repositories" do
+            assert_perform_request method: "HEAD", url: "test-docs" do
+              SearchReindexJob.perform_now
+            end
+          end
+        end
+      end
+    end
   end
 end
