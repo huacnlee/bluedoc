712365631212253b43071e2496f9955e09db4e5d
Jason Lee <huacnlee@gmail.com>
2019-03-28 11:09:06 +0800

    Ensure to create ElasticSearch index on Rails boot

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Ensure Search indexes
+[User, Group, Repository, Doc, Note].each do |klass|
+  unless klass.__elasticsearch__.index_exists?
+    klass.__elasticsearch__.create_index!
+  end
+end
