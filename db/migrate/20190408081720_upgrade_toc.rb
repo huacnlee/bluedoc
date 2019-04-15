9c9087045883cfb210923cb7815c60641aa7d06f
Jason Lee <huacnlee@gmail.com>
2019-04-15 18:44:59 +0800

    Ignore error on upgrade toc

--- db/migrate/20190408081720_upgrade_toc.rb
+++ db/migrate/20190408081720_upgrade_toc.rb
@@ -3,7 +3,11 @@ class UpgradeToc < ActiveRecord::Migration[6.0]
     puts "Upgrading the Old YAML style toc to tocs"
     Repository.all.each do |repo|
       puts "    repo: #{repo.id}"
-      Toc.create_by_toc_text!(repo)
+      begin
+        Toc.create_by_toc_text!(repo)
+      rescue
+        puts "    repo: #{repo.id} upgrade failed"
+      end
     end
   end
 end
