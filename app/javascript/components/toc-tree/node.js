f28d4587c16b8b32abb29a483ef9cfe1a180b0d5
guoquan.yang <guoquan.yang@5th.work>
2019-04-17 16:45:51 +0800

    Change i18n

--- app/javascript/components/toc-tree/node.js
+++ app/javascript/components/toc-tree/node.js
@@ -147,7 +147,7 @@ class Node extends Component {
               <ul className="dropdown-menu dropdown-menu-sw">
                 <li><a href={`${info.url}/edit`} className="dropdown-item">{t('.Edit doc')}</a></li>
                 <li className='dropdown-item' onClick={this.handleUpdate}>{t('.Setting Doc')}</li>
-                <li className='dropdown-item' onClick={this.handleCreate}>{t('.Create Doc')}</li>
+                <li className='dropdown-item' onClick={this.handleCreate}>{t('.Add a Doc inside')}</li>
                 <li className='dropdown-divider'></li>
                 <li className='dropdown-item' onClick={this.handleDelete}>{t('.Delete doc')}</li>
               </ul>
