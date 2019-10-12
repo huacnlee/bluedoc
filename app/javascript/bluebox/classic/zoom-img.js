ece66a18669365d64c87950ba237fc7668f42b58
Jason Lee <huacnlee@gmail.com>
2019-10-12 14:22:28 +0800

    Remove zooming, rewrite it.

--- app/javascript/bluebox/classic/zoom-img.js
+++ app/javascript/bluebox/classic/zoom-img.js
@@ -1,10 +1,39 @@
-import Zooming from 'zooming';
+class Zooming {
+  constructor() {
+    this.container = document.querySelector('.zoom-container');
+    if (!this.container) {
+      this.container = document.createElement('div');
+      this.container.classList.add('zoom-container');
+      document.body.appendChild(this.container);
+    }
+    this.container.addEventListener('click', (e) => {
+      e.preventDefault();
+      document.body.classList.toggle('zoom-img-in');
+    });
+  }
+
+  listen(elName) {
+    const els = document.querySelectorAll(elName);
+    els.forEach((el) => {
+      el.addEventListener('click', this.click);
+    });
+  }
+
+  click = (e) => {
+    e.preventDefault();
+    const el = e.target;
+    const newImg = document.createElement('img');
+    newImg.src = el.getAttribute('src');
+    this.container.innerHTML = `<div class="zoom-inner">${newImg.outerHTML}</div>`;
+    document.body.classList.toggle('zoom-img-in');
+    // Revert to top scroll
+    setTimeout(() => {
+      this.container.scrollTop = 0;
+    }, 10);
+  }
+}
 
 document.addEventListener('turbolinks:load', () => {
-  const zooming = new Zooming({
-    customSize: "50%",
-    transitionDuration: 0.2,
-    bgColor: "#fff",
-  });
+  const zooming = new Zooming();
   zooming.listen('.markdown-body img');
 });
