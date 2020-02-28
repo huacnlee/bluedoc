fd1c4c9e268ab559e55b579acfb68b69498b96ec
Jason Lee <huacnlee@gmail.com>
2020-02-28 20:24:16 +0800

    Implement MarkdownEditor image, file, video upload

--- app/javascript/bluedoc/editor/markdown-editor.js
+++ app/javascript/bluedoc/editor/markdown-editor.js
@@ -1,19 +1,80 @@
 import Toolbar from './markdown-toolbar';
+import { AttachmentUpload } from './attachment-upload';
+
+const CodeMirror = require('react-codemirror');
+require('codemirror/mode/markdown/markdown.js');
+require('codemirror/mode/gfm/gfm.js');
+require('codemirror/addon/mode/overlay.js');
+require('codemirror/addon/selection/mark-selection.js');
 
 export default class MarkdownEditor extends React.Component {
+  editorRef = React.createRef();
+
+  codeMirrorOptions = {
+    mode: 'gfm',
+  }
+
+  codemirror = null
+
   constructor(props) {
     super(props);
 
     const { title, value, format } = props;
+
     this.state = {
       title,
       value,
       format,
     };
+
+
+    const { directUploadURL, blobURLTemplate } = window.App;
+
+    this.attachmentService = {
+      imageUpload(file, onProgress) {
+        return new Promise((resolve, reject) => {
+          const upload = new AttachmentUpload(
+            file,
+            directUploadURL,
+            blobURLTemplate,
+            url => resolve(url),
+            onProgress,
+          );
+          upload.start();
+        });
+      },
+      attachmentUpload(file, onProgress) {
+        return new Promise((resolve, reject) => {
+          const upload = new AttachmentUpload(
+            file,
+            directUploadURL,
+            blobURLTemplate,
+            url => resolve(url),
+            onProgress,
+          );
+          upload.start();
+        });
+      },
+      videoUpload(file, onProgress) {
+        return new Promise((resolve, reject) => {
+          const upload = new AttachmentUpload(
+            file,
+            directUploadURL,
+            blobURLTemplate,
+            url => resolve(url),
+            onProgress,
+          );
+          upload.start();
+        });
+      },
+    };
   }
 
-  onChange = (e) => {
-    const newValue = e.target.value;
+  componentDidMount() {
+    this.codemirror = this.editorRef.current.getCodeMirror();
+  }
+
+  onChange = (newValue) => {
     this.props.onChange(newValue);
     this.setState({ value: newValue });
   }
@@ -32,6 +93,75 @@ export default class MarkdownEditor extends React.Component {
     return i18n.t(key);
   }
 
+  _insertText = (text) => {
+    const { codemirror } = this;
+    const { doc } = codemirror;
+
+    const cursor = doc.getCursor();
+    doc.replaceRange(text, cursor);
+  }
+
+  _insertImage = ({ name, url }) => {
+    const text = `![${name}](${url})`;
+    this._insertText(text);
+  }
+
+  _insertFile = ({ name, url }) => {
+    const text = `[${name}](${url})`;
+    this._insertText(text);
+  }
+
+  _insertVideo = ({ name, url }) => {
+    const text = `<video preload="no" title="${name}"><source src="${url}"></video>`;
+    this._insertText(text);
+  }
+
+  _uploadFileEvent = (event, fileType, next) => {
+    const service = this.attachmentService;
+    const { imageUpload, attachmentUpload, videoUpload } = service;
+    if (!imageUpload) return next();
+
+    const editor = this;
+
+    let transfer = { files: [] };
+
+    if (event.target && event.target.files) {
+      transfer = { files: event.target.files };
+    }
+
+    const { files } = transfer;
+
+    for (const file of files) {
+      const { type, name } = file;
+      event.preventDefault();
+      const onProgress = (progress) => { };
+
+      if (fileType === 'image') {
+        imageUpload(file, onProgress).then((url) => {
+          editor._insertImage({ url, name });
+        }).catch((e) => {
+          console.error(e);
+        });
+      } else if (fileType === 'video') {
+        videoUpload(file, onProgress)
+          .then((url) => {
+            editor._insertVideo({ url, name });
+          })
+          .catch((e) => {
+            console.error(e);
+          });
+      } else {
+        attachmentUpload(file, onProgress)
+          .then((url) => {
+            editor._insertFile({ url, name });
+          })
+          .catch((e) => {
+            console.error(e);
+          });
+      }
+    }
+  }
+
   render() {
     const { value } = this.state;
     let { title } = this.state;
@@ -44,7 +174,7 @@ export default class MarkdownEditor extends React.Component {
     }
 
     return <div className={`rich-editor-${mode} markdown-editor`}>
-      <Toolbar value={value} mode={mode} onChange={this.onChange} container={this} />
+      <Toolbar value={value} mode={mode} editor={this} />
       <div className="editor-bg" ref={this.containerRef}>
         <div className="editor-box">
           {mode === 'full' && (
@@ -57,8 +187,8 @@ export default class MarkdownEditor extends React.Component {
                 className="editor-title-text" />
             </div>
           )}
-          <div className="editor-text markdown-body">
-            <textarea class="editor-body-text" onChange={this.onChange}>{value}</textarea>
+          <div className="editor-text">
+            <CodeMirror className="editor-body-text" autoFocus options={this.codeMirrorOptions} ref={this.editorRef} value={value} onChange={this.onChange} />
           </div>
         </div>
       </div>
