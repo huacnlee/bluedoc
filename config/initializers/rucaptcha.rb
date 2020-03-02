e7165b21e3098a340bd1e9797ff5499da10edd60
Jason Lee <huacnlee@gmail.com>
2020-03-03 00:04:23 +0800

    Fix RuCaptcha config

---
+++ config/initializers/rucaptcha.rb
@@ -0,0 +1,5 @@
+redis_config = Rails.application.config_for(:redis)
+
+RuCaptcha.configure do
+  self.cache_store = [:redis_cache_store, { namespace: "rucaptcha-#{redis_config["namespace"]}", url: redis_config["url"], expires_in: 2.weeks }]
+end
