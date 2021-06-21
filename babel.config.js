e4ce2826013d2911e8539edd925e820408c5abd2
Jason Lee <huacnlee@gmail.com>
2021-06-21 15:47:23 +0800

    Upgrade for support Apple M1

--- babel.config.js
+++ babel.config.js
@@ -1,70 +1,71 @@
-module.exports = function(api) {
-  var validEnv = ['development', 'test', 'production']
-  var currentEnv = api.env()
-  var isDevelopmentEnv = api.env('development')
-  var isProductionEnv = api.env('production')
-  var isTestEnv = api.env('test')
+module.exports = function (api) {
+  var validEnv = ["development", "test", "production"];
+  var currentEnv = api.env();
+  var isDevelopmentEnv = api.env("development");
+  var isProductionEnv = api.env("production");
+  var isTestEnv = api.env("test");
 
   if (!validEnv.includes(currentEnv)) {
     throw new Error(
-      'Please specify a valid `NODE_ENV` or ' +
+      "Please specify a valid `NODE_ENV` or " +
         '`BABEL_ENV` environment variables. Valid values are "development", ' +
         '"test", and "production". Instead, received: ' +
         JSON.stringify(currentEnv) +
-        '.'
-    )
+        "."
+    );
   }
 
   return {
     presets: [
       isTestEnv && [
-        require('@babel/preset-env').default,
+        require("@babel/preset-env").default,
         {
           targets: {
-            node: 'current'
-          }
-        }
+            node: "current",
+          },
+        },
       ],
       (isProductionEnv || isDevelopmentEnv) && [
-        require('@babel/preset-env').default,
+        require("@babel/preset-env").default,
         {
           forceAllTransforms: true,
-          useBuiltIns: 'entry',
+          useBuiltIns: "entry",
           modules: false,
-          exclude: ['transform-typeof-symbol']
-        }
-      ]
+          exclude: ["transform-typeof-symbol"],
+        },
+      ],
     ].filter(Boolean),
     plugins: [
-      require('babel-plugin-macros'),
-      require('@babel/plugin-syntax-dynamic-import').default,
-      isTestEnv && require('babel-plugin-dynamic-import-node'),
-      require('@babel/plugin-transform-destructuring').default,
+      require("babel-plugin-macros"),
+      require("@babel/plugin-syntax-dynamic-import").default,
+      isTestEnv && require("babel-plugin-dynamic-import-node"),
+      require("@babel/plugin-transform-destructuring").default,
       [
-        require('@babel/plugin-proposal-class-properties').default,
+        require("@babel/plugin-proposal-class-properties").default,
         {
-          loose: true
-        }
+          loose: true,
+        },
       ],
       [
-        require('@babel/plugin-proposal-object-rest-spread').default,
+        require("@babel/plugin-proposal-object-rest-spread").default,
         {
-          useBuiltIns: true
-        }
+          useBuiltIns: true,
+        },
       ],
       [
-        require('@babel/plugin-transform-runtime').default,
+        require("@babel/plugin-transform-runtime").default,
         {
           helpers: false,
-          regenerator: true
-        }
+          regenerator: true,
+        },
       ],
       [
-        require('@babel/plugin-transform-regenerator').default,
+        require("@babel/plugin-transform-regenerator").default,
         {
-          async: false
-        }
-      ]
-    ].filter(Boolean)
-  }
-}
+          async: false,
+        },
+      ],
+      ["@babel/plugin-proposal-private-methods", { loose: true }],
+    ].filter(Boolean),
+  };
+};
